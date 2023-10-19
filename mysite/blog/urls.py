f47bfd029c83300c7ef18899679c75bf6aa9053a
Asilbek <asilbekpolatov77@gmail.com>
2023-10-19 11:34:22 +0500

    Ready to sell

---
+++ mysite/blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import *
+from .feeds import LatestPostsFeed
+
+app_name = 'blog'
+
+
+urlpatterns = [
+    path('', post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
+    path('<int:post_id>/share/',post_share, name='post_share'),
+    path('<int:post_id>/comment/',post_comment, name='post_comment'),
+    path('feed/',LatestPostsFeed(), name='post_feed'), 
+    path('search/', post_search, name='post_search'),
+    
+]
